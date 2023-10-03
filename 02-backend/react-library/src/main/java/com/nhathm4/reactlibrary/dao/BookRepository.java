f1457ba6d05a91ea43b2b915e27f1243dc5b55db
NhatHM4 <haminhnhat711@gmail.com>
2023-10-03 21:44:12 +0700

    add search

--- 02-backend/react-library/src/main/java/com/nhathm4/reactlibrary/dao/BookRepository.java
+++ 02-backend/react-library/src/main/java/com/nhathm4/reactlibrary/dao/BookRepository.java
@@ -1,8 +1,13 @@
 package com.nhathm4.reactlibrary.dao;
 
 import com.nhathm4.reactlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
